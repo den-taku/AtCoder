cf59cf95f8c21685549c1c0081cef34e1d78b268
DenTaku <dentaku1412@gmail.com>
2020-11-08 22:43:24 +0900

    Done abc182b

--- abc/abc182/src/bin/b.rs
+++ abc/abc182/src/bin/b.rs
@@ -1,6 +1,22 @@
 use competitive::prelude::*;
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(n: usize, mut a: [i64; n]) -> i64 {
+    let mut ans = 2;
+    let mut buf = 0;
+    a.sort();
+    let max = a[n-1];
+    for i in 2..max+1 {
+        let mut com = 0;
+        for j in 0..n {
+            if a[j] % i == 0 {
+                com += 1;
+            }
+        }
+        if buf <= com {
+            buf = com;
+            ans = i
+        }
+    }
+    ans
 }
