f4e66b8281db604e0c2cf981896f1b0b755e704b
DenTaku <dentaku1412@gmail.com>
2020-11-08 22:43:55 +0900

    TLE abc182d

--- abc/abc182/src/bin/d.rs
+++ abc/abc182/src/bin/d.rs
@@ -1,6 +1,14 @@
 use competitive::prelude::*;
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(n: usize, a: [i64; n]) -> i64 {
+    let mut ans = 0;
+    let mut pos = 0;
+    for i in 0..n {
+        for j in 0..i+1 {
+            pos += a[j];
+            ans = if ans >= pos { ans } else { pos };
+        }
+    }
+    ans
 }
