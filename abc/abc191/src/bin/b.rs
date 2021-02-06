a18f2162e3c89fabe5dc68e7cdd74657ed686382
DenTaku <dentaku1412@gmail.com>
2021-02-06 23:10:37 +0900

    AC

--- abc/abc191/src/bin/b.rs
+++ abc/abc191/src/bin/b.rs
@@ -1,6 +1,14 @@
 use competitive::prelude::*;
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(n: usize, x: i32, a: [i32; n]) {
+    let mut v = Vec::new();
+    for i in 0..n {
+        if a[i] != x {
+            v.push(a[i].clone())
+        }
+    }
+    for e in &v {
+        print!("{} ", e)
+    }
 }
