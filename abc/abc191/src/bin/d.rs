56ab8596ef1f358240ed14280028ae0fd318cad8
DenTaku <dentaku1412@gmail.com>
2021-02-06 23:11:10 +0900

    TRY

--- abc/abc191/src/bin/d.rs
+++ abc/abc191/src/bin/d.rs
@@ -1,6 +1,19 @@
 use competitive::prelude::*;
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(x: f32, y: f32, r: f32) -> i32 {
+    let low = (x - r).ceil() as i32;
+    let high = (x + r).floor() as i32;
+
+    let mut ans = 0;
+
+    for i in low..high + 1 {
+        let p = (r.powf(2.0) - (x - i as f32).powf(2.0)).sqrt();
+        let bottom = (y - p).ceil() as i32;
+        let top = (y + p).floor() as i32;
+        for _ in bottom..top + 1 {
+            ans += 1
+        }
+    }
+    ans
 }
--- abc/abc191/src/bin/f.rs
+++ abc/abc191/src/bin/f.rs
@@ -1,6 +1,23 @@
 use competitive::prelude::*;
+use std::collections::BTreeSet;
+
+fn gcd_set(a: Vec<usize>) -> BTreeSet<usize> {
+    let mut set = BTreeSet::new();
+    for i in 0..a.len() {
+        for j in i+1..a.len() {
+            set.insert(gcd(a[i], a[j]));
+        }
+    }
+    set
+}
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(n: usize, mut a: [usize; n]) -> usize {
+    let set = gcd_set(a.clone());
+    a.sort_by_key(|&x| Reverse(x));
+    if *set.iter().last().unwrap() == a.pop().unwrap() {
+        set.len() + 1
+    } else {
+        set.len() + 2
+    }
 }
