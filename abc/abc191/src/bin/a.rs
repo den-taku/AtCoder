6314be3c608b21dd17f227031fe6e26e01cfd166
DenTaku <dentaku1412@gmail.com>
2021-02-06 23:09:56 +0900

    AC

--- abc/abc191/src/bin/a.rs
+++ abc/abc191/src/bin/a.rs
@@ -1,6 +1,10 @@
 use competitive::prelude::*;
 
 #[argio(output = AtCoder)]
-fn main() {
-    unimplemented!();
+fn main(v: i32, t: i32, s: i32, d: i32) {
+    if v * t <= d && d <= v * s {
+        println!("No")
+    } else {
+        println!("Yes")
+    }
 }
